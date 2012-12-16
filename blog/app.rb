4a778c6dcb94095f766e2eae2fab8511d242b34c
Semyon Perepelitsa <sema@sema.in>
2012-12-17 00:09:21 +0800

    Web-interface for adding posts

--- blog/app.rb
+++ blog/app.rb
@@ -8,6 +8,19 @@ before do
   Maglev.abort_transaction
 end
 
+after do
+  Maglev.commit_transaction
+end
+
 get "/" do
   haml :index
+end
+
+get "/new" do
+  haml :new
+end
+
+post "/" do
+  $blog.add_post params[:title], params[:body]
+  redirect to("/")
 end
